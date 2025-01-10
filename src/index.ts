6c7ceb465fa805997be3553bd7fd5b635557cb2b
iamAyanBiswas <ayanbiswas9191@gmail.com>
2025-01-10 09:14:50 +0530

    import type change

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-function dynamicWebHeaders() {
-  return {
-    title: (title: string = ""):void => {
-      (document as Document).title = title
-    },
-    icon: (refLink:string):void => {
-      if(!refLink) throw new Error('refLink can not be empty')
-      if(typeof(refLink)==="string") throw new Error('refLink should be string')
-      let link = (document as Document).querySelector("link[rel*='icon']") as HTMLLinkElement || (document as Document).createElement('link');
-      link.type = 'image/x-icon';
-      link.rel = 'icon';
-      link.href = refLink;
-      document.head.appendChild(link);
-    }
-  }
+export const title = (title: string = ""): void => {
+  (document as Document).title = title
 }
+export const icon = (refLink: string): void => {
+  if (!refLink) throw new Error('refLink can not be empty')
+  if (typeof (refLink) !== "string") throw new Error('refLink should be string')
+  let link = (document as Document).querySelector("link[rel*='icon']") as HTMLLinkElement || (document as Document).createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'icon';
+  link.href = refLink;
+  document.head.appendChild(link);
+}
+
+const dynamicWebHeaders ={
+  title:title,
+  icon:icon,
+}
+
 
 export default dynamicWebHeaders
